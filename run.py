2f5a802e03f6c3662a2dc59cf0d180d201962f17
gallegodev <gallegodev1991@gmail.com>
2023-09-25 18:01:58 +0000

    Update portifolio 6 name

--- run.py
+++ run.py
@@ -10,14 +10,9 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route('/login')
 def login():
-    return render_template("login.html")
-
-
-
-
-
+    return render_template('login.html')
 
 
 if __name__ == "__main__":
