4192c813f07eeae4c27bb1ef4f122b2749c4374b
gallegodev <gallegodev1991@gmail.com>
2023-09-26 16:34:48 +0000

    Adjust code to change website concept for a blog concept

--- blog/views.py
+++ blog/views.py
@@ -1,35 +1,3 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Ticket
-from .forms import TicketForm
+from django.shortcuts import render
 
-def ticket_list(request):
-    tickets = Ticket.objects.all()
-    return render(request, 'ticket_app/ticket_list.html', {'tickets': tickets})
-
-def ticket_create(request):
-    if request.method == 'POST':
-        form = TicketForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('ticket_list')
-    else:
-        form = TicketForm()
-    return render(request, 'ticket_app/ticket_form.html', {'form': form})
-
-def ticket_update(request, pk):
-    ticket = get_object_or_404(Ticket, pk=pk)
-    if request.method == 'POST':
-        form = TicketForm(request.POST, instance=ticket)
-        if form.is_valid():
-            form.save()
-            return redirect('ticket_list')
-    else:
-        form = TicketForm(instance=ticket)
-    return render(request, 'ticket_app/ticket_form.html', {'form': form})
-
-def ticket_delete(request, pk):
-    ticket = get_object_or_404(Ticket, pk=pk)
-    if request.method == 'POST':
-        ticket.delete()
-        return redirect('ticket_list')
-    return render(request, 'ticket_app/ticket_confirm_delete.html', {'ticket': ticket})
+# Create your views here.
