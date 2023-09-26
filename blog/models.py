0995c446105a672861c0e446b7e88a53e1a3dea6
gallegodev <gallegodev1991@gmail.com>
2023-09-26 16:15:37 +0000

    Add ticket model to create, update and delete appointments

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Ticket(models.Model):
+    date = models.DateField()
+    description = models.TextField()
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Ticket
+from .forms import TicketForm
 
-# Create your views here.
+def ticket_list(request):
+    tickets = Ticket.objects.all()
+    return render(request, 'ticket_app/ticket_list.html', {'tickets': tickets})
+
+def ticket_create(request):
+    if request.method == 'POST':
+        form = TicketForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('ticket_list')
+    else:
+        form = TicketForm()
+    return render(request, 'ticket_app/ticket_form.html', {'form': form})
+
+def ticket_update(request, pk):
+    ticket = get_object_or_404(Ticket, pk=pk)
+    if request.method == 'POST':
+        form = TicketForm(request.POST, instance=ticket)
+        if form.is_valid():
+            form.save()
+            return redirect('ticket_list')
+    else:
+        form = TicketForm(instance=ticket)
+    return render(request, 'ticket_app/ticket_form.html', {'form': form})
+
+def ticket_delete(request, pk):
+    ticket = get_object_or_404(Ticket, pk=pk)
+    if request.method == 'POST':
+        ticket.delete()
+        return redirect('ticket_list')
+    return render(request, 'ticket_app/ticket_confirm_delete.html', {'ticket': ticket})
