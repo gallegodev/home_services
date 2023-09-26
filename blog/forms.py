ecbd5b8f0cadbf2cda72e0311e1a1e023a599e5a
gallegodev <gallegodev1991@gmail.com>
2023-09-26 16:54:43 +0000

    Add admin on blog

--- blog/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Ticket
-
-class TicketForm(forms.ModelForm):
-    class Meta:
-        model = Ticket
-        fields = ['date', 'description', 'name', 'email', 'phone_number']
